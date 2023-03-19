2ac7f035245ae5ce4a3331ba30859a5e81f37c61
aydincanaltun <aydncn99@gmail.com>
2023-03-19 17:21:23 +0300

    Problem 1'deki 2 URL içinde arka arkaya harita oluşturma

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using prolab21;
 using prolab21.Entity;
 using prolab21.Entity.Enums;
-
+Console.WriteLine("URL 1 MAP");
 Response<List<List<Block>>> map = await Maze.CreateMap(Constant.MapUrls[0]);
 if(map.IsSuccess){
     int height = map.Result.Count;
@@ -20,4 +20,26 @@ if(map.IsSuccess){
     }
 }else{
     Console.WriteLine(map.ErrorMessage);
+}
+
+Console.WriteLine("---------------");
+Console.WriteLine("URL 2 MAP");
+Response<List<List<Block>>> map2 = await Maze.CreateMap(Constant.MapUrls[0]);
+if(map2.IsSuccess){
+    int height = map2.Result.Count;
+    for(int i=0; i<height; i++){
+        int width = map2.Result[i].Count;
+        for(int j=0; j<width; j++){
+            char c;
+            if(map.Result[i][j].IsMoveble){
+                c = map2.Result[i][j].Type == BlockType.Path ? 'c' : 'a';
+            }else{
+                c = 'w';
+            }
+            Console.Write(string.Format("{0} ", c));
+        }
+        Console.Write('\n');
+    }
+}else{
+    Console.WriteLine(map2.ErrorMessage);
 }

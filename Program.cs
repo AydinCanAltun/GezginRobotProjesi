e8538b701064f8a2d60a4fe22aeb8352d3ae33ca
aydincanaltun <aydncn99@gmail.com>
2023-03-19 17:00:59 +0300

    Base Project Commited

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using prolab21;
+using prolab21.Entity;
+using prolab21.Entity.Enums;
+
+Response<List<List<Block>>> map = await Maze.CreateMap(Constant.MapUrls[0]);
+if(map.IsSuccess){
+    int height = map.Result.Count;
+    for(int i=0; i<height; i++){
+        int width = map.Result[i].Count;
+        for(int j=0; j<width; j++){
+            char c;
+            if(map.Result[i][j].IsMoveble){
+                c = map.Result[i][j].Type == BlockType.Path ? 'c' : 'a';
+            }else{
+                c = 'w';
+            }
+            Console.Write(string.Format("{0} ", c));
+        }
+        Console.Write('\n');
+    }
+}else{
+    Console.WriteLine(map.ErrorMessage);
+}

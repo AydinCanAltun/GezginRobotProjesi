b13c0aefcc658ecdade28733641632281a18b700
aydincanaltun <aydncn99@gmail.com>
2023-03-25 17:49:58 +0300

    comment kodlar kaldırıldı

--- Entity/Constant.cs
+++ Entity/Constant.cs
@@ -15,21 +15,5 @@ namespace GezginRobotProjesi.Entity
 
         public static MapSize MinimumSize = new MapSize(10, 10);
         public static MapSize MaximumSize = new MapSize(100, 100);
-
-/*
-        public static Direction[][] Priorities = new Direction[][]{
-            new Direction[] {Direction.West, Direction.North, Direction.East, Direction.South},
-            new Direction[] {Direction.East, Direction.South, Direction.West, Direction.North},
-            new Direction[] {Direction.North, Direction.East, Direction.South, Direction.West},
-            new Direction[] {Direction.South, Direction.West, Direction.North, Direction.East}
-        };
-*/
-        public static Direction[][] Priorities = new Direction[][]{
-            new Direction[] {Direction.East, Direction.North, Direction.West, Direction.South},
-            new Direction[] {Direction.West, Direction.South, Direction.East, Direction.North},
-            new Direction[] {Direction.South, Direction.East, Direction.North, Direction.West},
-            new Direction[] {Direction.North, Direction.West, Direction.South, Direction.East}
-        };
-
     }
 }
